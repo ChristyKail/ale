ae16a505f1834317ffaee638034312dbb61750a8
ChristyKail <68659286+ChristyKail@users.noreply.github.com>
2022-03-10 17:58:49 +0000

    Added manager system Changed the way INCLUDE actions handle missing columns

--- ale_macro.py
+++ ale_macro.py
@@ -4,11 +4,17 @@ import csv
 
 class AleMacro:
 
-    def __init__(self, macro_file, ale_obj: ale.Ale):
+    def __init__(self, macro_file, ale_obj: ale.Ale, manager=None):
 
+        self.manager = manager
         self.ale_obj = ale_obj
         self.macro_list = list_from_file(macro_file)
         self.execute_actions()
+
+    def log(self, message):
+
+        if self.manager:
+            self.manager.log(message)
 
     def execute_actions(self):
 
@@ -33,7 +39,7 @@ class AleMacro:
                 self.include(macro)
 
             else:
-                raise ValueError(f'{macro[0]}: unrecognized macro action')
+                raise AleMacroException(f'{macro[0]}: unrecognized macro action')
 
     def verify_macro(self, macro: list, length: int):
 
@@ -72,15 +78,20 @@ class AleMacro:
         if len(macro) < 2:
             raise AleMacroException(f'{macro} is not a valid action')
 
-        columns = macro[1:]
+        include = []
+        missing = []
 
-        for column in columns:
+        for column in macro[1:]:
 
-            if column not in self.ale_obj.dataframe.columns:
-                print(f'{column} could not be included. Missing in original')
-                columns.remove(column)
+            if column in self.ale_obj.dataframe.columns:
+                include.append(column)
+            else:
+                missing.append(column)
 
-        self.ale_obj.dataframe = self.ale_obj.dataframe[columns]
+        self.ale_obj.dataframe = self.ale_obj.dataframe[include]
+
+        if missing:
+            self.log("The following INCLUDE columns are missing:\n"+"\n".join(missing))
 
 
 class AleMacroException(Exception):
@@ -106,4 +117,4 @@ def list_from_file(macro_file):
 
 if __name__ == '__main__':
     test_ale = ale.Ale('AVID.ALE')
-    AleMacro('presets/No Macro.csv', test_ale)
+    AleMacro('presets/None.csv', test_ale)
